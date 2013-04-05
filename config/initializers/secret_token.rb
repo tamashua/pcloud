0ba23b8541891ae6f2643c9ab2680a62623b6121
Tamashua <web@tamashua.com>
2013-04-04 23:15:33 -0400

    install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pestcloud::Application.config.secret_token = '92403f81df076f6de299f946d781f8b1ed7283ce259a6b5261b76de2a6a6db09192e101f150e6c3b888a12117bd99198a08c38766e192248ede2d17158e7ee09'
